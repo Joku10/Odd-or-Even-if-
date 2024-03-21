a0bd042cb233e3ed36bdf4ad38f87232f6d2ef54
Lijo Jolly <lijojolly10@gmail.com>
2024-03-22 00:20:32 +0530

    Add files via upload

---
+++ Odd or Even.c
@@ -0,0 +1,17 @@
+//To find the number is even or odd
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("\nEnter a Number:");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("The number is even");
+    }
+
+    else
+    {
+        printf("The number is odd");
+    }
+}
